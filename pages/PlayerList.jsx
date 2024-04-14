7d8b0101f62db0437c28d529ac7480638dc730a3
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-04-14 17:00:49 +0000

    added playerDetails page

---
+++ pages/PlayerList.jsx
@@ -0,0 +1,64 @@
+import { Link } from "react-router-dom";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+const PlayerList = () => {
+  const playersData = [
+    {
+      id: 1,
+      name: "Player 1",
+      role: "Batsman",
+      country: "India",
+    },
+    {
+      id: 2,
+      name: "Player 2",
+      role: "Bowler",
+      country: "Australia",
+    },
+  ];
+  return (
+    <>
+      <Header />
+      <main className="container">
+        <h2 className="my-3">List of Cricket Players</h2>
+        <div className="">
+          {playersData.map((player) => {
+            return (
+              <div className="card col-md-6 my-3">
+                <div className="card-header">
+                  <h2>{player.name}</h2>
+                </div>
+                <div className="card-body">
+                  <div className="card-text">
+                    <p>
+                      <strong>Country: </strong>
+                      {player.country}
+                    </p>
+                    <p>
+                      <strong>Role: </strong>
+                      {player.role}
+                    </p>
+                    <Link
+                      to={`playerdetails/${player.id}`}
+                      className="btn btn-primary"
+                    >
+                      Details
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </main>
+      <br /> <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </>
+  );
+};
+
+export default PlayerList;
