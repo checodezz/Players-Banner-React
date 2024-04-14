5aff3c8dfe99d945635b3ff49cd4c56bf6c09543
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-04-14 16:35:55 +0000

    added homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 import "./App.css";
 import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 export default function App() {
   return (
     <>
       <Header />
-      <main>React ⚛️ + Vite ⚡ + Replit</main>;
+      <main>
+        <img
+          src="https://placehold.co/1600x400?text=Hello+World"
+          className="img-fluid"
+        />
+
+        <div className="text-center my-5">
+          <h2>Cricket Players Database</h2>
+          <p>Explore the profiles of your favorite cricket players.</p>
+          <Link to="/" className="btn btn-primary">
+            Explore Players
+          </Link>
+        </div>
+
+        <div className="text-center my-5 py-5">
+          <h2>Players Report</h2>
+          <p>
+            Explore our latest player report to gain insights into our ICC's
+            performance and growth.
+          </p>
+          <Link to="/" className="btn btn-primary">
+            View Report{" "}
+          </Link>
+        </div>
+      </main>
+      ;
     </>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
